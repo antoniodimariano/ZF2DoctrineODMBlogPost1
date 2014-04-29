28774a0a57b44182343b4861befaa7abce140740
Antonio Di Mariano <antonio.dimariano@gmail.com>
2014-04-29 18:11:35 +0200

    add Module.php

--- module/Demo/Module.php
+++ module/Demo/Module.php
@@ -1,7 +1,9 @@
 <?php
 namespace Demo;
 
-use Demo\Mapper\EntryMapper;
+use Zend\ModuleManager\Feature\ViewHelperProviderInterface;
+use Zend\Mvc\ModuleRouteListener;
+use Zend\Mvc\MvcEvent;
 
 class Module
 {
@@ -13,32 +15,70 @@ class Module
     public function getAutoloaderConfig()
     {
         return array(
+
+
             'Zend\Loader\StandardAutoloader' => array(
                 'namespaces' => array(
-                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                    // if we're in a namespace deeper than one level we need to fix the \ in the path
+                    __NAMESPACE__ => __DIR__ . '/src/' . str_replace('\\', '/' , __NAMESPACE__),
                 ),
             ),
         );
     }
-    
+    public function onBootstrap(MvcEvent $e)
+    {
+        /*
+         *   You may not need to do this if
+         *   you're doing it elsewhere in your application
+         *
+         */
+
+        $eventManager        = $e->getApplication()->getEventManager();
+        $moduleRouteListener = new ModuleRouteListener();
+        $moduleRouteListener->attach($eventManager);
+    }
+
+    public function getControllerConfig() {
+
+        return array(
+            'factories' => array(
+                /*
+                 * Our controller has some dependencies which can be injected in.
+                 * We use a Factory to do the job
+                 */
+                'Demo\Controller\Entry' => 'Demo\Controller\Factories\EntryControllerFactory',
+            ),
+        );
+
+    }
+
     public function getServiceConfig()
     {
         return array(
             //Our Entry takes not parameters
+
             'invokables' => array(
                 'DemoEntry' => 'Demo\Entity\Entry',
             ),
+
+            /*
+             * Our service and mapper have some dependencies which can be injected in.
+             * We use the a Factory for each ones
+             */
+
             'factories' => array(
-                //But the mapper has some dependencies which can be injected in
-                'DemoEntryMapper' => function($sm) {
-                    $dm = $sm->get('doctrine.documentmanager.odm_default');
-                    return new EntryMapper($dm, $dm->getRepository('Demo\Entity\Entry'));
-                },
+
+                'Demo\Service\EntryService'  => 'Demo\Service\Factories\EntryServiceFactory',
+                'Demo\Mapper\EntryMapper'    => 'Demo\Mapper\Factories\EntryMapperFactory',
+
             ),
+
             //The Entry entity should be unique
+
             'shared' => array(
                 'DemoEntry' => false
             ),
+
         );
     }
 }
